e74791050f3cff7a28ff139556c0f712af58327c
ym-han <hanyongming@gmail.com>
2025-01-21 00:45:10 +0800

    Remove toUpperSnakeCase

--- ts-apps/decision-logic-visualizer/src/lib/utils.ts
+++ ts-apps/decision-logic-visualizer/src/lib/utils.ts
@@ -13,15 +13,3 @@ export type CSSClass = string
 export type Styles = {
   [val: string]: CSSProperties | Styles
 }
-
-/*************************************************
- *************** Other misc utils ****************
- *************************************************/
-
-/* ====== String processing utils ============ */
-
-export function toUpperSnakeCase(input: string): string {
-  return input
-    .replace(/([a-z])([A-Z])/g, '$1_$2') // Insert underscore between lowercase and uppercase letters
-    .toUpperCase()
-}
