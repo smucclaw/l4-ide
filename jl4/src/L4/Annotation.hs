6e11e4bff8d37f21ac59f21790e7e838a215e578
Andres Loeh <andres@well-typed.com>
2025-01-20 08:30:30 +0100

    WIP.

--- jl4/src/L4/Annotation.hs
+++ jl4/src/L4/Annotation.hs
@@ -38,24 +38,25 @@ data CsnCluster_ t = CsnCluster
   deriving stock (Show, Ord, Eq, GHC.Generic)
   deriving anyclass ToExpr
 
-data AnnoElement_ t e
+data AnnoElement_ t
   = AnnoHole
   | AnnoCsn (CsnCluster_ t)
-  | AnnoExtra e
   deriving stock (Show, Ord, Eq, GHC.Generic)
   deriving anyclass ToExpr
 
-mkHoleWithType :: a -> AnnoElement_ t e
+mkHoleWithType :: a -> AnnoElement_ t
 mkHoleWithType _ = AnnoHole
 
-mkHole :: AnnoElement_ t e
+mkHole :: AnnoElement_ t
 mkHole = AnnoHole
 
-mkCsn :: CsnCluster_ t -> AnnoElement_ t e
+mkCsn :: CsnCluster_ t -> AnnoElement_ t
 mkCsn = AnnoCsn
 
 data Anno_ t e = Anno
-  { payload :: [AnnoElement_ t e]
+  { extra   :: e
+  , range   :: Maybe SrcRange
+  , payload :: [AnnoElement_ t]
   }
   deriving stock (Show, Ord, Eq, GHC.Generic)
   deriving anyclass ToExpr
@@ -63,10 +64,10 @@ data Anno_ t e = Anno
 csnTokens :: CsnCluster_ t -> [t]
 csnTokens cluster = cluster.payload.tokens <> cluster.trailing.tokens
 
-mkAnno :: [AnnoElement_ t e] -> Anno_ t e
-mkAnno es = Anno es
+mkAnno :: Monoid e => [AnnoElement_ t] -> Anno_ t e
+mkAnno es = Anno mempty Nothing es
 
-emptyAnno :: Anno_ t e
+emptyAnno :: Monoid e => Anno_ t e
 emptyAnno = mkAnno []
 
 isEmptyAnno :: Anno_ t e -> Bool
@@ -108,8 +109,8 @@ instance (Head xs ~ Anno' a, All c (Tail xs), xs ~ (Head xs : Tail xs)) => AnnoF
 -- Annotation Instances
 -- ----------------------------------------------------------------------------
 
-instance Semigroup (Anno_ t e) where
-  (Anno m1) <> (Anno m2) = Anno (m1 <> m2)
+instance Semigroup e => Semigroup (Anno_ t e) where
+  (Anno e1 r1 m1) <> (Anno e2 r2 m2) = Anno (e1 <> e2) (r1 <> r2) (m1 <> m2)
 
-instance Monoid (Anno_ t e) where
+instance Monoid e => Monoid (Anno_ t e) where
   mempty = emptyAnno
