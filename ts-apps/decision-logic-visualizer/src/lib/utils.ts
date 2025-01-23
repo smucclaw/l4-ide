6073c782032e6bc487a84618ce8690584a6dd141
ym-han <hanyongming@gmail.com>
2025-01-24 02:07:52 +0800

    Put Eq, Ord in utils.ts

--- ts-apps/decision-logic-visualizer/src/lib/utils.ts
+++ ts-apps/decision-logic-visualizer/src/lib/utils.ts
@@ -4,6 +4,30 @@ import type { Properties as CSSProperties } from 'csstype'
  *********** Types and type combinators **********
  *************************************************/
 
+/****************************
+     Pseudo Eq, Ord
+ *****************************/
+
+export interface Eq<T> {
+  isEqualTo(other: T): boolean
+}
+
+export enum ComparisonResult {
+  LessThan = -1,
+  Equal = 0,
+  GreaterThan = 1,
+}
+
+export interface Ord<T> extends Eq<T> {
+  compare(other: T): ComparisonResult
+}
+
+export function isLessThanOrEquals<A extends Ord<A>>(a: A, b: A): boolean {
+  return [ComparisonResult.Equal, ComparisonResult.LessThan].includes(
+    a.compare(b)
+  )
+}
+
 export type ValueOf<Obj> = Obj[keyof Obj]
 
 /* ====== CSS ============ */
