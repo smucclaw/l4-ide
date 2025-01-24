4995ef51b2ab5d5ea433e4b859b4bad335c00746
ym-han <hanyongming@gmail.com>
2025-01-25 01:38:26 +0800

    Add initial draft of component, along with a demo using a pre-defined IRExpr JSON object

---
+++ ts-apps/decision-logic-visualizer/src/lib/displayers/flow/flow.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import dagre from '@dagrejs/dagre'
+  import { getLayoutedElements, type DagreConfig } from './dagre'
+  import {
+    SvelteFlow,
+    Background,
+    ConnectionLineType,
+    type Node,
+    type Edge,
+  } from '@xyflow/svelte'
+  import {
+    FlowNode,
+    FlowEdge,
+    type FlowGraph,
+    type ExprFlowDisplayerProps,
+    exprLirNodeToAlgaUndirectedGraph,
+    algaUndirectedGraphToFlowGraph,
+  } from './types.svelte.ts'
+
+  import { onMount } from 'svelte'
+
+  import '@xyflow/svelte/dist/style.css'
+
+  const { context, node: exprLirNode }: ExprFlowDisplayerProps = $props()
+  // import { initialNodes, initialEdges } from './nodes-and-edges'
+
+  const flowGraph = algaUndirectedGraphToFlowGraph(
+    exprLirNodeToAlgaUndirectedGraph(context, exprLirNode)
+  )
+
+  const initialNodes = flowGraph.nodes
+    .toSorted((v1, v2) => -1 * v1.compare(v2))
+    .map((n) => n.toSFNodePojo())
+  const initialEdges = flowGraph.edges.map((e) => e.toSFEdgePojo())
+
+  /***********************************
+      SvelteFlow nodes and edges
+  ************************************/
+
+  let NODES = $state.raw<Node[]>(initialNodes)
+  let EDGES = $state.raw<Edge[]>(initialEdges)
+
+  /***********************************
+      Dagre Graph and Config
+  ************************************/
+
+  const dagreGraph = new dagre.graphlib.Graph()
+  dagreGraph.setDefaultEdgeLabel(() => ({}))
+
+  const dagreConfig: DagreConfig = {
+    dagreGraph: dagreGraph,
+    graph: {
+      direction: 'LR', // horizontal
+      // qn: how did they decide on these numbers?
+      // Doesn't matter though, since we'll use the measured width and height if available
+      defaultNodeWidth: 172,
+      defaultNodeHeight: 36,
+    },
+  }
+
+  onMount(() => {
+    const layoutedElements = getLayoutedElements(
+      dagreConfig,
+      initialNodes,
+      initialEdges
+    )
+    NODES = layoutedElements.nodes
+    EDGES = layoutedElements.edges
+
+    console.log(EDGES.length)
+    console.log('edges', EDGES)
+    console.log('onMounted!')
+  })
+</script>
+
+<div style="height:100vh;">
+  <SvelteFlow
+    bind:nodes={NODES}
+    bind:edges={EDGES}
+    fitView
+    connectionLineType={ConnectionLineType.SmoothStep}
+    defaultEdgeOptions={{ type: 'smoothstep', animated: false }}
+  >
+    <Background />
+  </SvelteFlow>
+</div>
--- ts-apps/decision-logic-visualizer/src/routes/+page.svelte
+++ ts-apps/decision-logic-visualizer/src/routes/+page.svelte
@@ -1,9 +1,105 @@
-<!-- Just trying to see if I can get the stokc example working -->
 <script lang="ts">
   import { SvelteFlowProvider } from '@xyflow/svelte'
-  import Flow from '$lib/displayers/flow-elk-stock/flow.svelte'
+  import { Either, Schema } from 'effect'
+  import { IRExpr, ExprLirSource } from '$lib/data/program-ir'
+  import { LirContext, LirRegistry, type LirRootType } from '$lib/layout-ir'
+
+  import ExprFlow from '$lib/displayers/flow/flow.svelte'
+
+  // Parse JSON object into IRExpr
+  const example1 = {
+    $type: 'BinExpr' as const,
+    op: 'And' as const,
+    left: {
+      $type: 'BinExpr' as const,
+      op: 'Or' as const,
+      left: {
+        $type: 'BoolVar' as const,
+        value: 'True' as const,
+        id: { id: 1 },
+        name: 'eats',
+      },
+      right: {
+        $type: 'BoolVar' as const,
+        value: 'Unknown' as const,
+        id: { id: 2 },
+        name: 'walks',
+      },
+      id: { id: 3 },
+    },
+    right: {
+      $type: 'BoolVar' as const,
+      value: 'True' as const,
+      id: { id: 4 },
+      name: 'swims',
+    },
+    id: { id: 5 },
+  }
+
+  const decode = Schema.decodeUnknownEither(IRExpr)
+  const eitherExpr = decode(example1)
+  let expr: IRExpr
+  if (Either.isRight(eitherExpr)) {
+    expr = eitherExpr.right
+  } else {
+    // not sure how to just unsafely coerce, so whatever
+    console.error('Decoding failed:', eitherExpr.left)
+    expr = {
+      $type: 'BoolVar',
+      value: 'True',
+      id: { id: 1 },
+      name: 'decoding somehow failed??!?!?!?!',
+    }
+  }
+
+  // Set up Lir
+  const registry = new LirRegistry()
+  const context = new LirContext()
+  const nodeInfo = { registry, context }
+
+  const exprLirNode = ExprLirSource.toLir(nodeInfo, expr)
+  // console.log(exprLirNode)
+  registry.setRoot(context, 'EXAMPLE_1', exprLirNode)
 </script>
 
+<h1>Decision Logic Visualizer draft</h1>
+<h2>
+  Examples of decision logic visualizations, starting from a 'json' of the
+  IRExpr that eventually gets transformed into a SvelteFlow graph
+</h2>
+<section>
+  <h3>Example 1</h3>
+  <p>Visualizing</p>
+  <pre><code>
+  {`const example1: IRExpr = {
+    $type: 'BinExpr' as const,
+    op: 'And',
+    left: {
+      $type: 'BinExpr' as const,
+      op: 'Or',
+      left: {
+        $type: 'BoolVar',
+        value: 'Unknown',
+        id: { id: 1 },
+        name: 'eats'
+      },
+      right: {
+        $type: 'BoolVar',
+        value: 'Unknown',
+        id: { id: 2 },
+        name: 'walks'
+      },
+      id: { id: 3 }
+    },
+    right: {
+      $type: 'BoolVar',
+      value: 'True',
+      id: { id: 4 },
+      name: 'swims'
+    },
+    id: { id: 5 }`}
+</code></pre>
+</section>
 <SvelteFlowProvider>
-  <Flow />
+  <ExprFlow {context} node={exprLirNode} />
 </SvelteFlowProvider>
