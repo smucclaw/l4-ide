e5f4e25c1f9f9f41ea20196a5e96bd7a42a95075
Meowyam <maryam@chilon.net>
2025-02-14 14:52:44 +0800

    fix using app.css from webview repo

--- ts-apps/webview/src/routes/+layout.svelte
+++ ts-apps/webview/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <!-- New to Svelte? See https://svelte.dev/tutorial/kit/layouts for how +layout.svelte is supposed to work -->
 
 <script lang="ts">
-  import '../app.css'
+  // it was using the app.css from the webview repo and not the decisionlogicvisualizer
+  // for now we're referencing it from decision-logic-visualizer
+  import '@repo/decision-logic-visualizer/src/app.css';
   let { children } = $props()
 </script>
 
--- ts-apps/webview/vite.config.ts
+++ ts-apps/webview/vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@repo/decision-logic-visualizer': path.resolve('../decision-logic-visualizer'),
+    },
+  },
   build: {
     rollupOptions: {
       external: ['vscode-webview'],
