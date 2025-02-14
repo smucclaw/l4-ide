379fd14bf94d53a0b8ca5623bc9aaff41832d1ac
ym-han <hanyongming@gmail.com>
2025-02-14 21:07:26 +0800

    Export DLV's style.css via `./dist/style.css` (basically something like @meowyam's exporting css in the files approach).
    Move the styles into a styles dir.

--- ts-apps/decision-logic-visualizer/src/routes/+layout.svelte
+++ ts-apps/decision-logic-visualizer/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <!-- New to Svelte? See https://svelte.dev/tutorial/kit/layouts for how +layout.svelte is supposed to work -->
 
 <script lang="ts">
-  import '../style.css'
+  import '../lib/styles/style.css'
   let { children } = $props()
 </script>
 
