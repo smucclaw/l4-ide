2212c249830cfadcf3ec698d8c4e3d5bc0585f71
ym-han <hanyongming@gmail.com>
2025-02-15 01:27:13 +0800

    Clean up

--- ts-apps/webview/vite.config.ts
+++ ts-apps/webview/vite.config.ts
@@ -1,17 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [sveltekit(), tailwindcss()],
-  resolve: {
-    // alias: {
-    //   '@repo/decision-logic-visualizer': path.resolve(
-    //     '../decision-logic-visualizer'
-    //   ),
-    // },
-  },
   build: {
     rollupOptions: {
       external: ['vscode-webview'],
