0785aa174c2b65ea3adf4d722d567ecae420e1a3
ym-han <hanyongming@gmail.com>
2025-02-14 20:09:15 +0800

    WIP: start on a simpler css setup for DLV

--- ts-apps/decision-logic-visualizer/vite.config.ts
+++ ts-apps/decision-logic-visualizer/vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import tailwindcss from '@tailwindcss/vite'
 import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss()],
+  plugins: [sveltekit()],
   ssr: {
     noExternal: ['@xyflow/svelte'],
   },
