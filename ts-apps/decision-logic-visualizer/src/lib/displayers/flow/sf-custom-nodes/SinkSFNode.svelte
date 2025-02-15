45135daea58812d7d46abc8f79b20bed5b27b941
ym-han <hanyongming@gmail.com>
2025-02-15 17:11:45 +0800

    Remove redundant style

--- ts-apps/decision-logic-visualizer/src/lib/displayers/flow/sf-custom-nodes/SinkSFNode.svelte
+++ ts-apps/decision-logic-visualizer/src/lib/displayers/flow/sf-custom-nodes/SinkSFNode.svelte
@@ -4,25 +4,14 @@ TODO: maybe reduce code duplication between this and SourceSFNode
 <script lang="ts">
   import { Handle } from '@xyflow/svelte'
   import type { NodeProps, Node } from '@xyflow/svelte'
-  import {
-    groupingNodehandleStyle,
-    defaultSFHandlesInfo,
-  } from '../types.svelte.js'
+  import { defaultSFHandlesInfo } from '../types.svelte.js'
   // eslint-disable-next-line no-empty-pattern
   let {}: NodeProps<Node> = $props()
 </script>
 
 <div class="grouping-node">
-  <Handle
-    type="source"
-    position={defaultSFHandlesInfo.sourcePosition}
-    style={groupingNodehandleStyle}
-  />
+  <Handle type="source" position={defaultSFHandlesInfo.sourcePosition} />
   <!-- The bit of text is there to improve the layouting -->
   <div style="opacity:0">-</div>
-  <Handle
-    type="target"
-    position={defaultSFHandlesInfo.targetPosition}
-    style={groupingNodehandleStyle}
-  />
+  <Handle type="target" position={defaultSFHandlesInfo.targetPosition} />
 </div>
