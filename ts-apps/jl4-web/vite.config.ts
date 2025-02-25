2cd9121ebd922432dacddb4c21a017e45221dbd0
Fendor <fendor@posteo.de>
2025-02-25 15:45:27 +0100

    Rename my-app to jl4-web

--- ts-apps/jl4-web/vite.config.ts
+++ ts-apps/jl4-web/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from "vite";
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [sveltekit()],
   worker: {
     format: "es",
   },
