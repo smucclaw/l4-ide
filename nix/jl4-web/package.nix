9041a610edd4b4cdf2c7c7eb13e6bd507a7e5983
mangoiv <contact@mangoiv.com>
2025-02-26 11:09:35 +0100

    [fix] hack to make build work for repo dependencies

--- nix/jl4-web/package.nix
+++ nix/jl4-web/package.nix
@@ -18,6 +18,10 @@ buildNpmPackage {
   buildPhase = ''
     runHook preBuild
     export VITE_BACKEND_URL=${if secure then "wss" else "ws"}://${url};
+    npm run build || true
+    # HACK: we need to build workspace dependencies but we don't care if
+    # not all of them succeed as long as enough succeed for the entire
+    # thing to build
     cd ./ts-apps/jl4-web
     npm run build
     runHook postBuild
