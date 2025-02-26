f04502e0e875b4988a0342ca91e1f7811d2b7d62
mangoiv <contact@mangoiv.com>
2025-02-26 14:05:16 +0100

    [chore] format web code

--- ts-apps/jl4-web/svelte.config.js
+++ ts-apps/jl4-web/svelte.config.js
@@ -9,14 +9,14 @@ const config = {
 
   kit: {
     adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		}),
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
     serviceWorker: {
       register: false,
     },
