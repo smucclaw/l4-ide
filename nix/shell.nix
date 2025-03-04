fa22567d4dae704de135fb004d9ac96246ff2c5b
mangoiv <contact@mangoiv.com>
2025-03-04 11:11:27 +0100

    [chore] some documentation and improvements for the nix support

---
+++ nix/shell.nix
@@ -0,0 +1,15 @@
+{
+  pkgs ? import <nixpkgs> { },
+  ...
+}:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.haskell.compiler.ghc98
+    pkgs.haskell.packages.ghc98.haskell-language-server
+    pkgs.cabal-install
+    pkgs.zlib
+    pkgs.nodejs
+    pkgs.typescript
+    pkgs.nixos-anywhere
+  ];
+}
