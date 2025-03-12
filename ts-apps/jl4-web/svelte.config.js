837330c18674cbddd5b8d7e283f5ad77ebe728fe
ym-han <hanyongming@gmail.com>
2025-03-12 16:37:52 +0800

    Fix deployment issue: set `fallback` in sveltekit config to `index.html`, and remove the previous fallback html page (#228)

--- ts-apps/jl4-web/svelte.config.js
+++ ts-apps/jl4-web/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
       // these options are set automatically — see below
       pages: "build",
       assets: "build",
-      fallback: "./src/error.html",
+      fallback: "index.html",
       precompress: false,
       strict: true,
     }),
