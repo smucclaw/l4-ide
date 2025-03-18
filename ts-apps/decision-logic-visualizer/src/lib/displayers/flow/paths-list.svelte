d4532f6efdba8001e410abf8d928a9b48ea18659
ym-han <hanyongming@gmail.com>
2025-03-18 21:49:11 +0800

    WIP: Putative attempt at using Shadcn-Svelte/BitsUI's Toggle for linearized Paths. Running into issues with styling

--- ts-apps/decision-logic-visualizer/src/lib/displayers/flow/paths-list.svelte
+++ ts-apps/decision-logic-visualizer/src/lib/displayers/flow/paths-list.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onDestroy } from 'svelte'
-  import { type PathListDisplayerProps } from './svelteflow-types.js'
+  import type { PathListDisplayerProps } from './svelteflow-types.js'
+  import { Toggle } from '$lib/ui-primitives/toggle/index.js'
 
   /************************
        Lir
@@ -18,23 +19,32 @@
     {#each node.getPaths(context) as path, pathIndex}
       <li class="grid grid-cols-[max-content_1fr] gap-x-2 items-center">
         <!-- Row number / path index -->
-        <div class="font-semibold px-3 max-w-[25px] text-right">
+        <div class="px-3 max-w-[25px] text-right">
           {pathIndex + 1}
         </div>
         <!-- TODO: Refactor the hover CSS to use our css vars -->
-        <button
-          class="rounded-md border-1 p-2 max-w-fit hover:bg-sky-100 text-xs text-left"
-          onmouseenter={() =>
-            path.highlightCorrespondingPathInLadderGraph(context)}
-          onmouseleave={() =>
-            path.unhighlightCorrespondingPathInLadderGraph(context)}
+        <Toggle
+          pressed={path.is$Selected()}
+          class="rounded-md border-1 p-2 max-w-fit text-xs text-left"
+          onPressedChange={(pressed: boolean) => {
+            if (pressed) {
+              path.highlightCorrespondingPathInLadderGraph(context)
+            } else {
+              path.unhighlightCorrespondingPathInLadderGraph(context)
+            }
+          }}
         >
           {path.toPretty(context)}
-        </button>
+        </Toggle>
       </li>
     {/each}
   </ul>
 </section>
+
+<!-- onmouseenter={() =>
+  path.highlightCorrespondingPathInLadderGraph(context)}
+onmouseleave={() =>
+  path.unhighlightCorrespondingPathInLadderGraph(context)} -->
 
 <style>
   .paths-list-content-wrapper {
