a5d8c7c55bf3a622525948d9685f7eba3b0a36bd
ym-han <hanyongming@gmail.com>
2025-03-20 20:33:08 +0800

    Clean up alga-dag.test.ts

--- ts-apps/decision-logic-visualizer/src/tests/alga-dag.test.ts
+++ ts-apps/decision-logic-visualizer/src/tests/alga-dag.test.ts
@@ -6,31 +6,8 @@ import {
   connect,
   connectNodeToSource,
   connectSinkToNode,
-} from '../lib/algebraic-graphs/dag.js' // Adjust the import path accordingly
-import type { Ord, HasId } from '$lib/utils.js'
-import { ComparisonResult } from '$lib/utils.js'
-
-class NumberWrapper implements Ord<NumberWrapper>, HasId {
-  constructor(private value: number) {}
-
-  isEqualTo(other: NumberWrapper): boolean {
-    return other instanceof NumberWrapper && this.value === other.value
-  }
-
-  compare(other: NumberWrapper): ComparisonResult {
-    if (this.value < other.value) return ComparisonResult.LessThan
-    if (this.value > other.value) return ComparisonResult.GreaterThan
-    return ComparisonResult.Equal
-  }
-
-  getId(): string {
-    return this.value.toString()
-  }
-
-  toString(): string {
-    return `NWrapper ${this.value}`
-  }
-}
+} from '../lib/algebraic-graphs/dag.js'
+import { NumberWrapper } from './number-wrapper.js'
 
 /*******************
       Overlay
