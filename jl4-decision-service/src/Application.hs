37da6c72e6fb4bcc044fe2280d74ebfd9bb9cdf3
Wong Meng Weng <mengwong@legalese.com>
2025-03-21 10:39:09 +0800

    add support for sourcePaths option, look for .l4 and .yaml

--- jl4-decision-service/src/Application.hs
+++ jl4-decision-service/src/Application.hs
@@ -7,6 +7,7 @@ module Application (defaultMain) where
 
 import Control.Concurrent.STM (newTVarIO)
 import Control.Monad.Trans.Reader (ReaderT (..))
+import Control.Monad (unless)
 import qualified Examples
 import Network.Wai
 import Network.Wai.Handler.Warp
@@ -17,6 +18,9 @@ import Schema
 import Servant
 import Servant.Swagger.UI (SwaggerSchemaUI, swaggerSchemaUIServer)
 import Server
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath (takeExtension, (</>))
+import qualified Data.Map as Map
 
 -- ----------------------------------------------------------------------------
 -- Option Parser
@@ -37,8 +41,16 @@ opts =
 
 defaultMain :: IO ()
 defaultMain = do
-  Options{port, serverName} <- execParser opts
-  dbRef <- newTVarIO Examples.functionSpecs
+  Options{port, serverName, sourcePaths} <- execParser opts
+
+  l4Files <- expandSourcePaths sourcePaths
+  unless (null sourcePaths) $ putStrLn $ "Choosing .l4 + .yaml pairs from: " <> show l4Files
+
+  l4Functions <- Examples.loadL4Functions l4Files
+  unless (null sourcePaths) $ putStrLn $ "** Loaded l4 functions from disk: " <> show (length l4Functions)
+  unless (null l4Functions) $ print $ Map.keys l4Functions
+
+  dbRef <- newTVarIO (Examples.functionSpecs <> l4Functions)
   let
     initialState = DbState dbRef
   putStrLn $ "Application started on port: " <> show port
@@ -46,6 +58,19 @@ defaultMain = do
     let
       settings = setPort port $ setLogger aplogger defaultSettings
     runSettings settings (app initialState serverName)
+expandSourcePaths :: [FilePath] -> IO [FilePath]
+expandSourcePaths paths = do
+  files <- concat <$> mapM expandPath paths
+  return $ filter (\f -> takeExtension f == ".l4") files
+
+expandPath :: FilePath -> IO [FilePath]
+expandPath path = do
+  isDir <- doesDirectoryExist path
+  if isDir
+    then do
+      contents <- listDirectory path
+      concat <$> mapM (expandPath . (path </>)) contents
+    else return [path]
 
 type ApiWithSwagger =
   SwaggerSchemaUI "swagger-ui" "swagger.json"
--- jl4-decision-service/src/Options.hs
+++ jl4-decision-service/src/Options.hs
@@ -9,6 +9,7 @@ import Options.Applicative
 data Options = Options
   { port :: Int
   , serverName :: Maybe Text.Text
+  , sourcePaths :: [FilePath]  -- New field for source files or directories
   }
 
 optionsParser :: Parser Options
@@ -28,5 +29,13 @@ optionsParser = do
               <> short 's'
               <> metavar "URL"
               <> help "Name of the server. Exposed in swagger.json field."
+          )
+      )
+    <*> many
+      ( strOption
+          ( long "sourcePaths"
+              <> short 'f'
+              <> metavar "SOURCE"
+              <> help "One or more files or directories containing L4 source files to expose"
           )
       )
