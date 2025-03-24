01ba344a92ec9ffd197ffbf478581c3038bd1262
Andres Löh <andres@well-typed.com>
2025-03-24 20:01:37 +0100

    Fix type checker (#273)
    * Fix a bug in the typechecker allowing infinite types.
    
    We have to be more aggressive in applying the substitution.
    There possibly are still problems left. We should probably
    apply the substitution eagerly in general rather than lazily
    as we mostly do now.
    
    * Add test cases.

--- jl4-core/src/L4/TypeCheck.hs
+++ jl4-core/src/L4/TypeCheck.hs
@@ -1448,8 +1448,20 @@ tryExpandTypeSynonym r args = do
 -- In general, types should be instantiated prior to unification, and
 -- kind-checking should not involve unification.
 --
--- One major complication are type synonyms which we have to expand
--- during unification. When do we want to expand a type synonym?
+-- Unify proceeds in multiple layers.
+--
+-- First, we have to substitute. (TODO: We should probably just apply
+-- the substitution eagerly all the time. There is a reason other
+-- implementations do this as well.) Why? Because we have to prevent
+-- infinite types from arising (which are usually unwanted, but more
+-- importantly even, can make the system loop very easily). And in
+-- order to detect cycles, we need to know the full set of inference
+-- variables that occur in the target type once we try to bind an
+-- inference variable. The easiest way to achieve this is to
+-- substitute first.
+--
+-- Next, we have to expand type synonyms.
+-- When do we want to expand a type synonym?
 --
 -- Basically whenever we have ruled out the inference variable cases,
 -- because if we have one inference variable against a type synonym,
@@ -1460,24 +1472,33 @@ tryExpandTypeSynonym r args = do
 -- expand, we handle the remaining cases in 'unifyBase'.
 --
 unify :: Type' Resolved -> Type' Resolved -> Check Bool
-unify (InfVar _ann1 _pre1 i1) t2@(InfVar _ann2 _pre2 i2)
+unify t1 t2 = do
+  t1' <- applySubst t1
+  t2' <- applySubst t2
+  unify' t1' t2'
+
+-- | Unify cases for inference variables, after substitution. Prevent
+-- infinite types by performing the so-called "occurs check".
+--
+unify' :: Type' Resolved -> Type' Resolved -> Check Bool
+unify' (InfVar _ann1 _pre1 i1) t2@(InfVar _ann2 _pre2 i2)
   | i1 == i2             = pure True
   | otherwise            = bind i1 t2
-unify (InfVar _ann1 _pre1 i1) t2
+unify' (InfVar _ann1 _pre1 i1) t2
   | i1 `elem` infVars t2 = pure False -- addError (OccursCheck t1 t2)
   | otherwise            = bind i1 t2
-unify t1 (InfVar _ann2 _pre2 i2)
+unify' t1 (InfVar _ann2 _pre2 i2)
   | i2 `elem` infVars t1 = pure False -- addError (OccursCheck t1 t2)
   | otherwise            = bind i2 t1
-unify t1 t2              = expandAndUnify t1 t2
+unify' t1 t2             = expandAndUnify t1 t2
 
 -- | Handles the cases where we've established we have no top-level
 -- inference variables.
 --
 expandAndUnify :: Type' Resolved -> Type' Resolved -> Check Bool
 expandAndUnify t1 t2 =
-  tryExpand t1 (\ t1' -> unify t1' t2) $
-  tryExpand t2 (\ t2' -> unify t1 t2') $
+  tryExpand t1 (\ t1' -> unify' t1' t2) $
+  tryExpand t2 (\ t2' -> unify' t1 t2') $
   unifyBase t1 t2
   where
     -- Tries to expand the given type synonym. If expansion succeeds,
@@ -1498,12 +1519,12 @@ unifyBase (TyApp _ann1 n1 ts1) (TyApp _ann2 n2 ts2) = do
   -- both are type constructors or type variables
   r <- ensureSameRef n1 n2
   -- We should not need to check the same length because we've done kind checking.
-  rs <- traverse (uncurry unify) (zip ts1 ts2)
+  rs <- traverse (uncurry unify') (zip ts1 ts2)
   pure (and (r : rs))
 unifyBase (Fun _ann1 onts1 t1) (Fun _ann2 onts2 t2)
   | length onts1 == length onts2 = do
-    rs <- traverse (uncurry unify) (zip (optionallyNamedTypeType <$> onts1) (optionallyNamedTypeType <$> onts2))
-    r <- unify t1 t2
+    rs <- traverse (uncurry unify') (zip (optionallyNamedTypeType <$> onts1) (optionallyNamedTypeType <$> onts2))
+    r <- unify' t1 t2
     pure (and (r : rs))
 unifyBase (Type _ann1) (Type _ann2) = pure True
 unifyBase _t1 _t2 = pure False -- addError (UnificationError t1 t2)
@@ -1518,12 +1539,8 @@ infVars (InfVar _ _ i)  = [i]
 
 bind :: Int -> Type' Resolved -> Check Bool
 bind i t = do
-  subst <- use #substitution
-  case Map.lookup i subst of
-    Nothing -> do
-      assign #substitution (Map.insert i t subst)
-      pure True
-    Just t' -> unify t' t -- addError (InternalUnificationErrorBind i t t')
+  modifying' #substitution (Map.insert i t)
+  pure True
 
 severity :: CheckErrorWithContext -> Severity
 severity (MkCheckErrorWithContext e _) =
