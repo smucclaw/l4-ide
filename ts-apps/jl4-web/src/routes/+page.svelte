c0ce439ba82cd884307424a8ef284fbf90c1860f
ym-han <hanyongming@gmail.com>
2025-03-28 17:51:07 +0800

    Set a max width of 96% for jl4-webview pane and center it

--- ts-apps/jl4-web/src/routes/+page.svelte
+++ ts-apps/jl4-web/src/routes/+page.svelte
@@ -332,7 +332,7 @@ DECIDE \`is a British citizen (variant)\` IS
   </Resizable.Pane>
   <Resizable.Handle style="width: 10px;" />
   <Resizable.Pane>
-    <div id="jl4-webview" class="h-full bg-white">
+    <div id="jl4-webview" class="h-full max-w-[96%] mx-auto bg-white">
       {#if declLirNode}
         <!-- TODO: Think more about whether to use #key -- which destroys and rebuilds the component --- or have flow-base work with the reactive node prop -->
         {#key declLirNode}
