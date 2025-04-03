b512a909ef96b8068ad52523d7eda4b4a0015d0e
ym-han <hanyongming@gmail.com>
2025-04-03 21:15:41 +0800

    Distinguish between Subst and Environment

--- ts-apps/decision-logic-visualizer/src/lib/eval/environment.ts
+++ ts-apps/decision-logic-visualizer/src/lib/eval/environment.ts
@@ -2,9 +2,57 @@ import type { Unique } from '@repo/viz-expr'
 import type { Value } from './type.js'
 import type { LirId } from '../layout-ir/core.js'
 
+export class Environment {
+  #env: Subst
+
+  constructor(initial: Array<Value>) {
+    this.#env = new Subst(initial)
+  }
+
+  get(unique: Unique): Value | undefined {
+    return this.#env.get(unique)
+  }
+
+  set(unique: Unique, value: Value) {
+    this.#env.set(unique, value)
+  }
+
+  getUniques(): Array<Unique> {
+    return this.#env.getUniques()
+  }
+
+  getEntries(): Array<[Unique, Value]> {
+    return this.#env.getEntries()
+  }
+
+  getFresh(): Unique {
+    return this.#env.getMaxUnique() + 1
+  }
+
+  clone() {
+    return new Environment(this.#env.asArray())
+  }
+
+  getClonedEnvExtendedWith(other: Array<[Unique, Value]>) {
+    const newEnv = this.clone()
+    for (const [uniq, value] of other) {
+      newEnv.set(uniq, value)
+    }
+    return newEnv
+  }
+}
+
 /** A mapping from Unique to Value */
 export class Subst {
   #subst: Array<Value>
+
+  static fromEntries(entries: Array<[Unique, Value]>) {
+    const initial: Array<Value> = []
+    entries.forEach(([unique, value]) => {
+      initial[unique] = value
+    })
+    return new Subst(initial)
+  }
 
   constructor(initial: Array<Value>) {
     this.#subst = initial
@@ -20,12 +68,24 @@ export class Subst {
     return this.#subst[unique]
   }
 
+  getMaxUnique(): Unique {
+    return this.#subst.length - 1
+  }
+
   getUniques(): Array<Unique> {
     return this.#subst.map((_, i) => i)
   }
 
   getEntries(): Array<[Unique, Value]> {
     return this.#subst.map((value, i) => [i, value])
+  }
+
+  asArray(): Array<Value> {
+    return this.#subst.slice()
+  }
+
+  clone() {
+    return new Subst(this.#subst.slice())
   }
 }
 
