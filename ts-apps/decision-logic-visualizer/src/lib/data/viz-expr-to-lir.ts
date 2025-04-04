bc8b0aba0485c74ac57b56db36b9d873a0592302
ym-han <hanyongming@gmail.com>
2025-04-04 20:12:24 +0800

    Keep track of the IRExpr ID => Dag<Lir> mapping; thread it through the traversal

--- ts-apps/decision-logic-visualizer/src/lib/data/viz-expr-to-lir.ts
+++ ts-apps/decision-logic-visualizer/src/lib/data/viz-expr-to-lir.ts
@@ -1,4 +1,4 @@
-import type { IRDecl, IRExpr } from '@repo/viz-expr'
+import type { IRDecl, IRExpr, IRId } from '@repo/viz-expr'
 /*
 Do not use $lib for the layout-ir imports
 */
@@ -51,14 +51,14 @@ export const LadderGraphLirSource: LirSource<IRExpr, LadderGraphLirNode> = {
     const overallSource = vertex(new SourceNoAnnoLirNode(nodeInfo).getId())
     const overallSink = vertex(new SinkLirNode(nodeInfo).getId())
 
-    const middle = transform(nodeInfo, expr)
+    const { graph: middle, vizExprToLir } = transform(nodeInfo, new Map(), expr)
 
     const dag = overallSource
       .connect(middle.getSource())
       .overlay(middle)
       .overlay(middle.getSink().connect(overallSink))
 
-    const ladderGraph = new LadderGraphLirNode(nodeInfo, dag)
+    const ladderGraph = new LadderGraphLirNode(nodeInfo, dag, vizExprToLir)
 
     // 2. Augment with explanatory edge labels (TODO: Not sure this shld happen here)
     augmentEdgesWithExplanatoryLabel(nodeInfo.context, ladderGraph)
@@ -70,11 +70,18 @@ export const LadderGraphLirSource: LirSource<IRExpr, LadderGraphLirNode> = {
 // TODO2: Attach a group id to the label for the flownode to make it easier to debug
 // TODO3: Return the number of groups as metadata in the FlowGraph
 
+export interface ToLirResult {
+  graph: DirectedAcyclicGraph<LirId>
+  vizExprToLir: Map<IRId, DirectedAcyclicGraph<LirId>>
+}
+
 /** Internal helper */
 function transform(
   nodeInfo: LirNodeInfo,
+  /** vizExprToLirEnv */
+  env: Map<IRId, DirectedAcyclicGraph<LirId>>,
   expr: IRExpr
-): DirectedAcyclicGraph<LirId> {
+): ToLirResult {
   /*
   One of the insights behind the ladder diagram, in particular, behind Meng's `layman`:
     Reduce visual clutter, by creating dummy source and sink nodes between which to nest the subgraphs, and only connecting those.
@@ -100,28 +107,55 @@ function transform(
   return match(expr)
     .with({ $type: 'BoolVar' }, (originalVar) => {
       const boolvar = new BoolVarLirNode(nodeInfo, originalVar)
-      return vertex(boolvar.getId())
+      const graph = vertex(boolvar.getId())
+      const newEnv = new Map(env).set(originalVar.id, graph)
+      return { graph, vizExprToLir: newEnv }
     })
     .with({ $type: 'Not' }, (neg) => {
-      const negand = transform(nodeInfo, neg.negand)
+      const { graph: negand, vizExprToLir: negandEnv } = transform(
+        nodeInfo,
+        env,
+        neg.negand
+      )
       const notStart = vertex(new NotStartLirNode(nodeInfo, negand).getId())
       const notEnd = vertex(new NotEndLirNode(nodeInfo).getId())
-      return notStart
+
+      const notGraph = notStart
         .connect(negand.getSource())
         .overlay(negand)
         .overlay(negand.getSink().connect(notEnd))
+      const newEnv = new Map([...env, ...negandEnv])
+
+      return { graph: notGraph, vizExprToLir: newEnv }
     })
     .with({ $type: 'And' }, (andExpr) => {
-      const childGraphs = andExpr.args.map((arg) => transform(nodeInfo, arg))
+      const childResults = andExpr.args.map((arg) =>
+        transform(nodeInfo, env, arg)
+      )
+      const childGraphs = childResults.map((result) => result.graph)
 
-      return childGraphs.reduceRight((acc, left) => {
+      const combinedGraph = childGraphs.reduceRight((acc, left) => {
         const accSource = acc.getSource()
         const leftSink = left.getSink()
         return leftSink.connect(accSource).overlay(left).overlay(acc)
       })
+
+      const allEnvs = [
+        env,
+        ...childResults.map((result) => result.vizExprToLir),
+      ]
+      const combinedEnv = new Map(
+        allEnvs.reduceRight(
+          (accEntries, env) => [...env, ...accEntries],
+          [] as [IRId, DirectedAcyclicGraph<LirId>][]
+        )
+      )
+
+      return { graph: combinedGraph, vizExprToLir: combinedEnv }
     })
     .with({ $type: 'Or' }, (orExpr) => {
-      const children = orExpr.args.map((n) => transform(nodeInfo, n))
+      const childResults = orExpr.args.map((n) => transform(nodeInfo, env, n))
+      const childGraphs = childResults.map((result) => result.graph)
 
       const overallSource = vertex(
         new SourceWithOrAnnoLirNode(
@@ -131,16 +165,30 @@ function transform(
       )
       const overallSink = vertex(new SinkLirNode(nodeInfo).getId())
 
-      const leftEdges = children.map((child) =>
+      const leftEdges = childGraphs.map((child) =>
         overallSource.connect(child.getSource())
       )
-      const rightEdges = children.map((child) =>
+      const rightEdges = childGraphs.map((child) =>
         child.getSink().connect(overallSink)
       )
 
-      return [overallSource, ...leftEdges, ...children, ...rightEdges].reduce(
-        overlay
+      const orGraph = [
+        overallSource,
+        ...leftEdges,
+        ...childGraphs,
+        ...rightEdges,
+      ].reduce(overlay)
+
+      // Envs
+      const childEnvs = childResults.map((result) => result.vizExprToLir)
+      const newEnv = new Map(
+        childEnvs.reduceRight(
+          (entries, env) => [...env, ...entries],
+          [] as [IRId, DirectedAcyclicGraph<LirId>][]
+        )
       )
+
+      return { graph: orGraph, vizExprToLir: newEnv }
     })
     .exhaustive()
 }
