d84cc1aeaebfd341cd58108d1adb95197d8933f3
ym-han <hanyongming@gmail.com>
2025-04-09 19:53:28 +0800

    Refactor: Distinguish more clearly between ladder and non-ladder-graph lir nodes.
    Make a `layout-ir/ladder-graph` subdir; move ladder lir stuff to that. Factor paths list lir out of ladder graph lir file.

--- ts-apps/decision-logic-visualizer/src/lib/layout-ir/ladder-graph/ladder.svelte.ts
+++ ts-apps/decision-logic-visualizer/src/lib/layout-ir/ladder-graph/ladder.svelte.ts
@@ -6,12 +6,12 @@ import {
   FalseVal,
   UnknownVal,
   veExprToEvExpr,
-} from '../eval/type.js'
+} from '../../eval/type.js'
 import type { IRExpr, BoolVar, Unique, Name, IRId } from '@repo/viz-expr'
-import { Assignment, Corefs } from '../eval/assignment.js'
+import { Assignment, Corefs } from '../../eval/assignment.js'
 import { Evaluator } from '$lib/eval/eval.js'
-import type { LirId, LirNode, LirNodeInfo } from './core.js'
-import { LirContext, DefaultLirNode } from './core.js'
+import type { LirId, LirNode, LirNodeInfo } from '../core.js'
+import { LirContext, DefaultLirNode } from '../core.js'
 import type { Ord } from '$lib/utils.js'
 import { ComparisonResult } from '$lib/utils.js'
 import {
@@ -19,23 +19,26 @@ import {
   isVertex,
   isOverlay,
   isConnect,
-  overlay,
-  empty,
   type Overlay,
   type Connect,
   type Vertex,
   type DirectedAcyclicGraph,
-} from '../algebraic-graphs/dag.js'
+} from '../../algebraic-graphs/dag.js'
 import {
   type Edge,
   DirectedEdge,
   type EdgeStylesContainer,
   type EdgeAttributes,
-} from '../algebraic-graphs/edge.js'
+} from '../../algebraic-graphs/edge.js'
 import type {
   Dimensions,
   BundlingNodeDisplayerData,
 } from '$lib/displayers/flow/svelteflow-types.js'
+import {
+  ValidPathsListLirNode,
+  InvalidPathsListLirNode,
+  type PathsListLirNode,
+} from '../paths-list.js'
 import { match, P } from 'ts-pattern'
 import _ from 'lodash'
 
@@ -104,90 +107,8 @@ export class FunDeclLirNode extends DefaultLirNode implements LirNode {
 }
 
 /*************************************************
-          Paths List, Lin Path
+          Lin Path
  *************************************************/
-
-export type PathsListLirNode = InvalidPathsListLirNode | ValidPathsListLirNode
-
-export function isInvalidPathsListLirNode(
-  node: PathsListLirNode
-): node is InvalidPathsListLirNode {
-  return node instanceof InvalidPathsListLirNode
-}
-
-export class InvalidPathsListLirNode extends DefaultLirNode implements LirNode {
-  constructor(nodeInfo: LirNodeInfo) {
-    super(nodeInfo)
-  }
-
-  toString(): string {
-    return 'INVALID_PATHS_LIST_LIR_NODE'
-  }
-
-  getChildren(_context: LirContext) {
-    return []
-  }
-
-  dispose(context: LirContext) {
-    context.clear(this.getId())
-  }
-}
-
-export function isValidPathsListLirNode(
-  node: PathsListLirNode
-): node is ValidPathsListLirNode {
-  return node instanceof ValidPathsListLirNode
-}
-
-export class ValidPathsListLirNode extends DefaultLirNode implements LirNode {
-  private paths: Array<LirId>
-
-  constructor(
-    nodeInfo: LirNodeInfo,
-    protected ladderGraph: LadderGraphLirNode,
-    paths: Array<LinPathLirNode>
-  ) {
-    super(nodeInfo)
-    this.paths = paths.map((n) => n.getId())
-  }
-
-  getPaths(context: LirContext) {
-    return this.paths.map((id) => context.get(id)) as Array<LinPathLirNode>
-  }
-
-  highlightPaths(context: LirContext, paths: LirId[]) {
-    const pathLirNodes = paths.map((id) =>
-      context.get(id)
-    ) as Array<LinPathLirNode>
-
-    // 1. Get the subgraph to be highlighted
-    // Exploits the property that (G, +, ε) is an idempotent monoid
-    const graphToHighlight = pathLirNodes
-      .map((p) => p._getRawPath())
-      .reduceRight(overlay, empty())
-
-    // 2. Reset edge styles wrt highlighting on ladder graph, then add highlight style to the subgraph
-    this.ladderGraph.clearHighlightEdgeStyles(context)
-    this.ladderGraph.highlightSubgraph(context, graphToHighlight)
-  }
-
-  getChildren(context: LirContext) {
-    return this.getPaths(context)
-  }
-
-  dispose(context: LirContext) {
-    // Dispose members
-    this.getChildren(context).map((n) => n.dispose(context))
-    this.paths = []
-
-    // Dispose self
-    context.clear(this.getId())
-  }
-
-  toString(): string {
-    return 'VALID_PATHS_LIST_LIR_NODE'
-  }
-}
 
 /** The simplest version of the LinPathLirNode -- no distinguishing between
  * compatible and incompatible linearized paths
