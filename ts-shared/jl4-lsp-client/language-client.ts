c46c93183462f194fbeaeed4510ec5ae08f156a7
ym-han <hanyongming@gmail.com>
2025-04-29 02:29:13 +0800

    Remove `stop` since there's already `dispose`. Clean up the Disposable interface and `deactivate`.

--- ts-shared/jl4-lsp-client/language-client.ts
+++ ts-shared/jl4-lsp-client/language-client.ts
@@ -1,9 +1,8 @@
-import type { Disposable } from 'vscode-languageclient'
 import { EvalAppRequestParams, EvalAppResult } from '@repo/viz-expr'
+import { IAmDisposable } from './utils.js'
 
-export interface L4LanguageClient extends Disposable {
+export interface L4LanguageClient extends IAmDisposable {
   start(): Promise<void>
-  stop(): Promise<void>
 
   sendEvalAppRequest(params: EvalAppRequestParams): Promise<EvalAppResult>
 }
