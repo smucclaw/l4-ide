03a4396f514b0690b8bda7d034525203c2c5fcab
ym-han <hanyongming@gmail.com>
2025-04-29 17:07:06 +0800

    Clean up; rename LspResult to LspResponse

--- ts-shared/jl4-lsp-client/language-client.ts
+++ ts-shared/jl4-lsp-client/language-client.ts
@@ -1,8 +1,14 @@
-import { EvalAppRequestParams, EvalAppResult } from '@repo/viz-expr'
+import type {
+  LspResponse,
+  EvalAppRequestParams,
+  EvalAppResult,
+} from './custom-protocol.js'
 import { IAmDisposable } from './utils.js'
 
 export interface L4LanguageClient extends IAmDisposable {
   start(): Promise<void>
 
-  sendEvalAppRequest(params: EvalAppRequestParams): Promise<EvalAppResult>
+  sendEvalAppRequest(
+    params: EvalAppRequestParams
+  ): Promise<LspResponse<EvalAppResult>>
 }
