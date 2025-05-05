e8a803bdd9785d000b8d607a346fa8e33d89e1cf
ym-han <hanyongming@gmail.com>
2025-05-05 22:10:43 +0800

    Add L4Connection class

---
+++ ts-apps/decision-logic-visualizer/src/lib/l4-connection.ts
@@ -0,0 +1,23 @@
+import type { LadderBackendApi } from 'jl4-client-rpc'
+import { EvalAppRequestType } from 'jl4-client-rpc'
+import { EvalAppRequestParams } from '@repo/viz-expr'
+
+/** Higher-level wrapper around functionality provided by the Ladder backend. */
+export class L4Connection {
+  constructor(private readonly api: LadderBackendApi) {}
+
+  /** Evaluate an App with actual arguments on the backend. */
+  async evalApp(
+    appExpr: EvalAppRequestParams['appExpr'],
+    args: EvalAppRequestParams['args'],
+    verTxtDocId: EvalAppRequestParams['verTxtDocId']
+  ) {
+    const params: EvalAppRequestParams = {
+      $type: 'EvalAppRequestParams',
+      appExpr,
+      args,
+      verTxtDocId,
+    }
+    return this.api.sendClientRequest(EvalAppRequestType, params)
+  }
+}
