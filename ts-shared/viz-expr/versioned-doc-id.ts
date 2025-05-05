383055d3a4bafbe97ece269ef987c6d4aae2dae3
ym-han <hanyongming@gmail.com>
2025-05-05 20:24:16 +0800

    TS frontend: Add comment on why we have an Effect schema for the lsp VersionedTextDocumentIdentifier

--- ts-shared/viz-expr/versioned-doc-id.ts
+++ ts-shared/viz-expr/versioned-doc-id.ts
@@ -3,6 +3,11 @@ import { Schema } from 'effect'
 
 /** This is just the VersionedTextDocumentIdentifier as an Effect schema.
  * https://github.com/microsoft/vscode-languageserver-node/blob/df05883f34b39255d40d68cef55caf2e93cff35f/types/src/main.ts#L1797
+ *
+ * I'm writing an Effect Schema that's iso to the LSP type using the LSP type directly,
+ * because that seems necessary to write an Effect schema for EvalAppRequestParams.
+ * But perhaps we should just give up on having an Effect schema
+ * for EvalAppRequestParams.
  */
 export const VersionedDocId = Schema.Struct({
   uri: Schema.String,
