f71fa1c3751a87b0f01985e5a125f03215cab87e
ym-han <hanyongming@gmail.com>
2025-05-17 23:55:11 +0800

    Do Ladder client RPC stuff (e.g. adding types and Effect schemas for inlining exprs) for inlining exprs

--- ts-apps/decision-logic-visualizer/src/lib/l4-connection.ts
+++ ts-apps/decision-logic-visualizer/src/lib/l4-connection.ts
@@ -1,11 +1,31 @@
+import type {
+  InlineExprsRequestParams,
+  InlineExprsSuccess,
+} from '@repo/viz-expr'
 import type { EvalAppResult, LadderBackendApi } from 'jl4-client-rpc'
-import { EvalAppRequestType, EvalAppRequestParams } from 'jl4-client-rpc'
+import {
+  EvalAppRequestType,
+  EvalAppRequestParams,
+  InlineExprsRequestType,
+} from 'jl4-client-rpc'
 
 /** Higher-level wrapper around functionality provided by the Ladder backend.
  * The software design here was inspired by VSCode-Lean's 'EditorConnection' and 'EditorApi'.
  */
 export class L4Connection {
   constructor(private readonly api: LadderBackendApi) {}
+
+  /** Inline exprs with the given Uniques */
+  async inlineExprs(
+    uniques: InlineExprsRequestParams['uniques'],
+    verDocId: InlineExprsRequestParams['verDocId']
+  ): Promise<InlineExprsSuccess | null> {
+    const params: InlineExprsRequestParams = {
+      uniques,
+      verDocId,
+    }
+    return this.api.sendClientRequest(InlineExprsRequestType, params)
+  }
 
   /** Evaluate an App with actual arguments on the backend. */
   async evalApp(
