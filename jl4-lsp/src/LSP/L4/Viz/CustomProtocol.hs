2d647ec2df8d44139649f6250c03907e5730faba
ym-han <hanyongming@gmail.com>
2025-05-19 22:39:25 +0800

    CustomProtocol.hs: EvalAppResult and InlineExprsSuccess ToJSON encoding. Add custom method type etc for inlining exprs
    Formatting / clean up

--- jl4-lsp/src/LSP/L4/Viz/CustomProtocol.hs
+++ jl4-lsp/src/LSP/L4/Viz/CustomProtocol.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE DataKinds #-}
 
 {-| Custom extensions to the LSP and payload types for JL4
-    See also @ts-shared/jl4-client-rpc/custom-protocol.ts@
+  See also 
+  - @ts-shared/jl4-client-rpc/custom-protocol.ts@ for RPC-related TS types
+  - @ts-shared/viz-expr/eval-on-backend.ts@ for the params/response payload TS types
 -}
 module LSP.L4.Viz.CustomProtocol where
 
 import qualified Base.Text as T
-import Data.Aeson   (FromJSON, ToJSON)
+import Data.Aeson (FromJSON(..), ToJSON(..), object, (.:), (.=), withObject)
 import GHC.Generics (Generic)
 import Data.Proxy (Proxy(..))
 import GHC.TypeLits  (Symbol, KnownSymbol, symbolVal)
@@ -34,15 +36,57 @@ data EvalAppRequestParams = EvalAppRequestParams
 -- | l4/evalApp response payload
 newtype EvalAppResult = EvalAppResult
   { value :: V.UBoolValue }
+  deriving stock (Eq, Show)
+
+instance ToJSON EvalAppResult where
+  toJSON (EvalAppResult val) = object [
+    "$type" .= ("EvalAppResult" :: T.Text),
+    "value" .= val
+    ]
+
+instance FromJSON EvalAppResult where
+  parseJSON = withObject "EvalAppResult" $ \obj -> do
+    typeTag <- obj .: "$type"
+    if typeTag == ("EvalAppResult" :: T.Text)
+      then EvalAppResult <$> obj .: "value"
+      else fail $ "Expected $type field to be 'EvalAppResult' but got: " <> T.unpack typeTag
+
+------------------------------------------------------
+-- l4/inlineExprs Request Params and Response Payload
+------------------------------------------------------
+
+-- | Payload / params for InlineExprsRequest
+data InlineExprsRequestParams = InlineExprsRequestParams
+  { uniques  :: [Int]
+  , verDocId :: LSP.VersionedTextDocumentIdentifier
+  }
   deriving stock (Eq, Show, Generic)
   deriving anyclass (FromJSON, ToJSON)
 
+-- | l4/inlineExprs response payload
+-- (Keeping it simple for now: just want to know whether the request was successful)
+data InlineExprsSuccess = InlineExprsSuccess
+  deriving stock (Eq, Show)
+
+instance ToJSON InlineExprsSuccess where
+  toJSON _ = object ["$type" .= ("InlineExprsSuccess" :: T.Text)]
+
+instance FromJSON InlineExprsSuccess where
+  parseJSON = withObject "InlineExprsSuccess" $ \obj -> do
+    typeTag <- obj .: "$type"
+    if typeTag == "InlineExprsSuccess"
+      then pure InlineExprsSuccess
+      else fail $ "Expected $type field to be 'InlineExprsSuccess' but got: " <> typeTag
+
 ------------------------------------------------------
---  l4/evalApp custom method for LSP
+--  Custom methods for LSP
 ------------------------------------------------------
 
 type EvalAppMethodName :: Symbol
 type EvalAppMethodName = "l4/evalApp"
+
+type InlineExprsMethodName :: Symbol
+type InlineExprsMethodName = "l4/inlineExprs"
 
 ------------------------------------------------------
 --  IsCustomMethod typeclass
@@ -52,7 +96,9 @@ class KnownSymbol a => IsCustomMethod (a :: Symbol) where
   getMethodName :: Proxy a -> T.Text
   getMethodName = T.pack . symbolVal
 
-instance IsCustomMethod EvalAppMethodName------------------------------------------------------
+instance IsCustomMethod EvalAppMethodName
+instance IsCustomMethod InlineExprsMethodName
+
 ------------------------------------------------------
 --  IsLadderRequestParams typeclass
 ------------------------------------------------------
