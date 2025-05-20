2cc0026b0a9dea971dd5701d7729d34c71e09cf8
fendor <fendor@posteo.de>
2025-05-20 17:09:31 +0200

    Implement desugaring of infix binary functions
    Desugars the arithmetic operators `+`, `-`, `*`, `/` and `modulo` to
    prefix functions:
    
    * `__PLUS__`
    * `__MINUS__`
    * `__TIMES__`
    * `__DIVIDE__`
    * `__MODULO__`
    
    Users can provide implementations for these functions to overload the
    infix binary operators for user-defined types.

--- jl4-core/src/L4/Desugar.hs
+++ jl4-core/src/L4/Desugar.hs
@@ -1,24 +1,26 @@
 module L4.Desugar (
   doDesugarModule,
-  DesugarError(..),
+  DesugarError (..),
   prettyDesugarError,
 )
 where
 
 import Base
 
-import qualified Data.Text as Text
 import L4.Annotation
-import L4.Syntax
 import L4.Print (prettyLayout)
-import qualified Base.Text as Text
+import L4.Syntax
 import qualified L4.TypeCheck.Environment as TypeCheck
+
+import qualified Base.Text as Text
+import Data.List.Split (splitWhen)
+import Optics
 
 newtype Desugar a = MkDesugar (Either DesugarError a)
   deriving newtype (Functor, Applicative, Monad)
 
 data HoleInfo
-  = HoleInfo { expected :: !Int, got :: !Int}
+  = HoleInfo {expected :: !Int, got :: !Int}
 
 data DesugarError where
   InternalAnnoRewritingError :: Expr Name -> HoleInfo -> DesugarError
@@ -44,7 +46,7 @@ dsSection (MkSection ann mName maka topdecl) =
     <$> traverse dsTopDecl topdecl
 
 dsTopDecl :: TopDecl Name -> Desugar (TopDecl Name)
-dsTopDecl = \case
+dsTopDecl = \ case
   Declare ann d -> Declare ann <$> dsDeclare d
   Decide ann d -> Decide ann <$> dsDecide d
   Assume ann d -> Assume ann <$> dsAssume d
@@ -56,7 +58,7 @@ dsImport :: Import Name -> Desugar (Import Name)
 dsImport imp = pure imp
 
 dsDirective :: Directive Name -> Desugar (Directive Name)
-dsDirective = \case
+dsDirective = \ case
   StrictEval ann expr ->
     StrictEval ann <$> dsExpr expr
   LazyEval ann expr ->
@@ -70,7 +72,7 @@ dsDirective = \case
       <*> traverse dsExpr events
 
 dsAssume :: Assume Name -> Desugar (Assume Name)
-dsAssume = \case
+dsAssume = \ case
   MkAssume ann tySig appForm mty ->
     pure $ MkAssume ann tySig appForm mty
 
@@ -85,103 +87,103 @@ dsDeclare (MkDeclare ann tySig appform tyDecl) =
 
 dsExpr :: Expr Name -> Desugar (Expr Name)
 dsExpr expr = case expr of
-    And ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ And ann e1' e2'
-    Or ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Or ann e1' e2'
-    RAnd ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ RAnd ann e1' e2'
-    ROr ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ ROr ann e1' e2'
-    Implies ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Implies ann e1' e2'
-    Equals ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Equals ann e1' e2'
-    Not ann e -> do
-      e' <- dsExpr e
-      pure $ Not ann e'
-    Plus ann e1 e2 -> do
-      args <- rewriteBinOpAnno expr (e1 :| [e2])
-      dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.plusName) args
-    Minus ann e1 e2 -> do
-      args <- rewriteBinOpAnno expr (e1 :| [e2])
-      dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.minusName) args
-    Times ann e1 e2 -> do
-      args <- rewriteBinOpAnno expr (e1 :| [e2])
-      dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.timesName) args
-    DividedBy ann e1 e2 -> do
-      args <- rewriteBinOpAnno expr (e1 :| [e2])
-      dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.divideName) args
-    Modulo ann e1 e2 -> do
-      args <- rewriteBinOpAnno expr (e1 :| [e2])
-      dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.moduloName) args
-    Cons ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Cons ann e1' e2'
-    Leq ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Leq ann e1' e2'
-    Lt ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Lt ann e1' e2'
-    Gt ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Gt ann e1' e2'
-    Geq ann e1 e2 -> do
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ Geq ann e1' e2'
-    Proj ann e1 n -> do
-      e1' <- dsExpr e1
-      pure $ Proj ann e1' n
-    Var ann v -> do
-      pure $ Var ann v
-    Lam ann sig body -> do
-      body' <- dsExpr body
-      pure $ Lam ann sig body'
-    App ann n ns -> do
-      ns' <- traverse dsExpr ns
-      pure $ App ann n ns'
-    AppNamed ann n ns order -> do
-      pure $ AppNamed ann n ns order
-    IfThenElse ann b e1 e2 -> do
-      b' <- dsExpr b
-      e1' <- dsExpr e1
-      e2' <- dsExpr e2
-      pure $ IfThenElse ann b' e1' e2'
-    Regulative ann r -> do
-      r' <- dsObligation r
-      pure $ Regulative ann r'
-    Consider ann e branches  -> do
-      e' <- dsExpr e
-      branches' <- traverse dsBranch branches
-      pure $ Consider ann e' branches'
-    e@Lit{} -> do
-      pure e
-    List ann es -> do
-      es' <- traverse dsExpr es
-      pure $ List ann es'
-    Where ann e lcl -> do
-      e' <- dsExpr e
-      lcl' <- traverse dsLocalDecl lcl
-      pure $ Where ann e' lcl'
-    Event ann e -> Event ann <$> dsEvent e
+  And ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ And ann e1' e2'
+  Or ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Or ann e1' e2'
+  RAnd ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ RAnd ann e1' e2'
+  ROr ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ ROr ann e1' e2'
+  Implies ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Implies ann e1' e2'
+  Equals ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Equals ann e1' e2'
+  Not ann e -> do
+    e' <- dsExpr e
+    pure $ Not ann e'
+  Plus ann e1 e2 -> do
+    args <- rewriteBinOpAnno expr e1 e2
+    dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.plusName) args
+  Minus ann e1 e2 -> do
+    args <- rewriteBinOpAnno expr e1 e2
+    dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.minusName) args
+  Times ann e1 e2 -> do
+    args <- rewriteBinOpAnno expr e1 e2
+    dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.timesName) args
+  DividedBy ann e1 e2 -> do
+    args <- rewriteBinOpAnno expr e1 e2
+    dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.divideName) args
+  Modulo ann e1 e2 -> do
+    args <- rewriteBinOpAnno expr e1 e2
+    dsExpr $ App (annoNoFunName ann args) (MkName emptyAnno $ rawName TypeCheck.moduloName) args
+  Cons ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Cons ann e1' e2'
+  Leq ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Leq ann e1' e2'
+  Lt ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Lt ann e1' e2'
+  Gt ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Gt ann e1' e2'
+  Geq ann e1 e2 -> do
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ Geq ann e1' e2'
+  Proj ann e1 n -> do
+    e1' <- dsExpr e1
+    pure $ Proj ann e1' n
+  Var ann v -> do
+    pure $ Var ann v
+  Lam ann sig body -> do
+    body' <- dsExpr body
+    pure $ Lam ann sig body'
+  App ann n ns -> do
+    ns' <- traverse dsExpr ns
+    pure $ App ann n ns'
+  AppNamed ann n ns order -> do
+    pure $ AppNamed ann n ns order
+  IfThenElse ann b e1 e2 -> do
+    b' <- dsExpr b
+    e1' <- dsExpr e1
+    e2' <- dsExpr e2
+    pure $ IfThenElse ann b' e1' e2'
+  Regulative ann r -> do
+    r' <- dsObligation r
+    pure $ Regulative ann r'
+  Consider ann e branches -> do
+    e' <- dsExpr e
+    branches' <- traverse dsBranch branches
+    pure $ Consider ann e' branches'
+  e@Lit{} -> do
+    pure e
+  List ann es -> do
+    es' <- traverse dsExpr es
+    pure $ List ann es'
+  Where ann e lcl -> do
+    e' <- dsExpr e
+    lcl' <- traverse dsLocalDecl lcl
+    pure $ Where ann e' lcl'
+  Event ann e -> Event ann <$> dsEvent e
  where
   annoNoFunName a as =
     fixAnnoSrcRange
@@ -225,7 +227,6 @@ dsBranch = \ case
   Otherwise ann e -> do
     de <- dsExpr e
     pure $ Otherwise ann de
-
 
 -- | Rewrite the 'Anno' of the given arguments @'NonEmpty' ('Expr' 'Name)'@ to
 -- include the concrete syntax nodes of the 'Anno' in the @'Expr' 'Name'@.
@@ -276,42 +277,27 @@ dsBranch = \ case
 -- @
 --   [["(", _, " PLUS "], [_, ")"]]
 -- @
-rewriteBinOpAnno :: Expr Name -> NonEmpty (Expr Name) -> Desugar [Expr Name]
-rewriteBinOpAnno expr allExprArgs = go annoPieces [] allExprArgs
-  where
+rewriteBinOpAnno :: Expr Name -> Expr Name -> Expr Name -> Desugar [Expr Name]
+rewriteBinOpAnno expr e1 e2 = do
+  let
+
+  case csnSlices of
+    [beforeFirst, beforeSecond, after] ->
+      pure
+        [ updateAnnoOf e1 (surroundWithCsn beforeFirst beforeSecond)
+        , updateAnnoOf e2 (surroundWithCsn [] after)
+        ]
+    _slices -> internalError $ InternalAnnoRewritingError expr (HoleInfo 2 numberOfAnnoHoles)
+ where
   annoPieces = (getAnno expr).payload
 
-  go :: [AnnoElement] -> [AnnoElement] -> NonEmpty (Expr Name) -> Desugar [Expr Name]
-  go pieces leadingCsn (arg :| args) =
-    case pieces of
-      (AnnoHole{} : rest) ->
-        let
-          (csns, holeAndCsn) = span (isJust . preview #_AnnoCsn) rest
-          rewrittenArg = setAnno (surroundWithCsn (getAnno arg) (reverse leadingCsn) csns) arg
-        in
-          case holeAndCsn of
-            [] -> case args of
-              [] -> pure [rewrittenArg]
-              (_ : _) -> internalError $ InternalAnnoRewritingError expr (HoleInfo (length allExprArgs) numberOfAnnoHoles)
-            _ -> case args of
-              [] -> internalError $ InternalAnnoRewritingError expr (HoleInfo (length allExprArgs) numberOfAnnoHoles)
-              (x : xs) -> do
-                rewrittenArgs <- go holeAndCsn [] (x :| xs)
-                pure (rewrittenArg : rewrittenArgs)
-      (csn@AnnoCsn{} : rest) ->
-        -- Accumulate the leading concrete syntax nodes.
-        go rest (csn:leadingCsn) (arg :| args)
-      [] ->
-        -- No concrete syntax nodes  to attach any more, so just return the rest.
-        -- Strictly speaking, this is incorrect, there is an implicit assumption
-        -- that the number of 'Hole's matches the number of arguments, otherwise
-        -- the incoming 'Anno' is wrong.
-        pure (arg:args)
+  updateAnnoOf e f = e & annoOf %~ f
 
+  csnSlices = splitWhen (isJust . preview #_AnnoHole) annoPieces
   numberOfAnnoHoles = length $ filter (isJust . preview #_AnnoHole) annoPieces
 
-  surroundWithCsn :: Anno -> [AnnoElement] -> [AnnoElement] -> Anno
-  surroundWithCsn a before after =
+  surroundWithCsn :: [AnnoElement] -> [AnnoElement] -> Anno -> Anno
+  surroundWithCsn before after a =
     fixAnnoSrcRange
       Anno
         { extra = a.extra
@@ -320,7 +306,7 @@ rewriteBinOpAnno expr allExprArgs = go annoPieces [] allExprArgs
         }
 
 prettyDesugarError :: DesugarError -> Text
-prettyDesugarError = \case
+prettyDesugarError = \ case
   InternalAnnoRewritingError context errorInfo ->
     Text.unlines
       [ "Internal Error:"
