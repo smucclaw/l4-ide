8fee9b679a320e4aa2187840495431db7f3182a8
ym-han <hanyongming@gmail.com>
2025-05-20 19:15:32 +0800

    Ladder frontend: Augment LadderApi with a way to update the LadderFlow component. Refactor L4Connection to use that in the `inlineExprs` high-level method

--- ts-apps/jl4-web/src/lib/ladder-api-for-monaco.ts
+++ ts-apps/jl4-web/src/lib/ladder-api-for-monaco.ts
@@ -4,12 +4,20 @@ import type {
   LadderBackendApi,
 } from 'jl4-client-rpc'
 import type { MonacoL4LanguageClient } from '$lib/monaco-l4-language-client'
+import type { RenderAsLadderInfo } from '@repo/viz-expr'
 
 /** Implementation of {@link LadderBackendApi} that is used by the Monaco/jl4-web version. */
 export class LadderApiForMonaco implements LadderBackendApi {
   constructor(
     /** Internal language client for direct communication with the language server */
-    private readonly languageClient: MonacoL4LanguageClient
+    private readonly languageClient: MonacoL4LanguageClient,
+
+    /** Helper to re-create the LadderFlow component.
+     * This should be closed over LadderEnv.
+     * This is basically the 'updating state in parent from child with a callback prop from parent' pattern */
+    private readonly makeLadderFlow: (
+      ladderInfo: RenderAsLadderInfo
+    ) => Promise<void>
   ) {}
 
   async sendClientRequest<P extends object, R>(
@@ -17,5 +25,10 @@ export class LadderApiForMonaco implements LadderBackendApi {
     params: P
   ): Promise<LspResponse<R>> {
     return await this.languageClient.sendRequest(type, params)
+  }
+
+  /** Update the LadderFlow component */
+  async updateViz(ladderInfo: RenderAsLadderInfo): Promise<void> {
+    await this.makeLadderFlow(ladderInfo)
   }
 }
