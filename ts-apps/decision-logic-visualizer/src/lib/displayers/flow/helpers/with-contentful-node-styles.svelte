3bfc82956b2bfc40627973928431edcf798f5108
ym-han <hanyongming@gmail.com>
2025-05-27 02:42:52 +0800

    Add `with-contentful-node-styles.svelte`: Wrapper that provides the base styles for contentful nodes

---
+++ ts-apps/decision-logic-visualizer/src/lib/displayers/flow/helpers/with-contentful-node-styles.svelte
@@ -0,0 +1,16 @@
+<!-- Wrapper that provides the base styles for contentful nodes -->
+<script lang="ts" module>
+  import type { Snippet } from 'svelte'
+
+  interface WithContentfulNodeStylesProps {
+    children: Snippet
+  }
+</script>
+
+<script lang="ts">
+  let { children }: WithContentfulNodeStylesProps = $props()
+</script>
+
+<div class="base-sf-node-styles transition-opacity duration-300 relative">
+  {@render children()}
+</div>
