cad325d8d6c98b27d49bbaebfdb8b35caf73070d
ym-han <hanyongming@gmail.com>
2025-05-31 00:30:05 +0800

    Remove some shadcn-svelte context menu ui primitives / components that we are very unlikely to use anytime soon

--- ts-apps/decision-logic-visualizer/src/lib/ui-primitives/context-menu/index.ts
+++ ts-apps/decision-logic-visualizer/src/lib/ui-primitives/context-menu/index.ts
@@ -2,12 +2,10 @@ import { ContextMenu as ContextMenuPrimitive } from 'bits-ui'
 
 import Trigger from './context-menu-trigger.svelte'
 import Group from './context-menu-group.svelte'
-import RadioGroup from './context-menu-radio-group.svelte'
 import Item from './context-menu-item.svelte'
 import GroupHeading from './context-menu-group-heading.svelte'
 import Content from './context-menu-content.svelte'
 import Shortcut from './context-menu-shortcut.svelte'
-import RadioItem from './context-menu-radio-item.svelte'
 import Separator from './context-menu-separator.svelte'
 import SubContent from './context-menu-sub-content.svelte'
 import SubTrigger from './context-menu-sub-trigger.svelte'
@@ -27,10 +25,8 @@ export {
   Content,
   Shortcut,
   Separator,
-  RadioItem,
   SubContent,
   SubTrigger,
-  RadioGroup,
   CheckboxItem,
   //
   Root as ContextMenu,
@@ -41,9 +37,7 @@ export {
   Content as ContextMenuContent,
   Trigger as ContextMenuTrigger,
   Shortcut as ContextMenuShortcut,
-  RadioItem as ContextMenuRadioItem,
   Separator as ContextMenuSeparator,
-  RadioGroup as ContextMenuRadioGroup,
   SubContent as ContextMenuSubContent,
   SubTrigger as ContextMenuSubTrigger,
   CheckboxItem as ContextMenuCheckboxItem,
