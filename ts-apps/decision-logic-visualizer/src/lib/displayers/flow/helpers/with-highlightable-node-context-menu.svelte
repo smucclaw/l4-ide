ba1b66784ba49cd8347705b37721bbd4508f95d2
ym-han <hanyongming@gmail.com>
2025-05-31 02:49:46 +0800

    Add simple way to highlight edges of paths by using context menu for uboolvar nodes. No state synchronization with pathslist

--- ts-apps/decision-logic-visualizer/src/lib/displayers/flow/helpers/with-highlightable-node-context-menu.svelte
+++ ts-apps/decision-logic-visualizer/src/lib/displayers/flow/helpers/with-highlightable-node-context-menu.svelte
@@ -4,13 +4,14 @@
 
   interface HighlightableNodeContextMenuProps {
     children: Snippet
+    onSelect?: () => void
   }
 </script>
 
 <script lang="ts">
   import * as ContextMenu from '$lib/ui-primitives/context-menu/index.js'
 
-  let { children }: HighlightableNodeContextMenuProps = $props()
+  let { children, onSelect }: HighlightableNodeContextMenuProps = $props()
 </script>
 
 <ContextMenu.Root>
@@ -18,6 +19,7 @@
     {@render children()}
   </ContextMenu.Trigger>
   <ContextMenu.Content>
-    <ContextMenu.Item inset>Highlight</ContextMenu.Item>
+    <!-- We can consider using a CheckboxItem in the future -->
+    <ContextMenu.Item inset {onSelect}>Toggle Highlight</ContextMenu.Item>
   </ContextMenu.Content>
 </ContextMenu.Root>
