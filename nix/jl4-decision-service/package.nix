348b7c9a6791e2fe3c5168a4ee9fee83f271a4c2
Wong Meng Weng <mengwong@legalese.com>
2025-06-19 14:02:50 +0800

    nix: attempt to vendor the needed l4 files using postInstall

--- nix/jl4-decision-service/package.nix
+++ nix/jl4-decision-service/package.nix
@@ -6,4 +6,24 @@ let
   };
   jl4-decision-service = hpkgs.callCabal2nix "jl4-decision-service" ../../jl4-decision-service {  };
 in
-hlib.doJailbreak jl4-decision-service
+hlib.doJailbreak (hlib.overrideCabal jl4-decision-service (oldAttrs: {
+  postInstall = ''
+    # Create data directory if it doesn't exist
+    mkdir -p $out/share/jl4
+    mkdir -p $out/share/doc
+    
+    # Copy jl4/experiments directory
+    if [ -d ../../jl4/experiments ]; then
+      cp -r ../../jl4/experiments $out/share/jl4/
+    else
+      echo "Warning: ../../jl4/experiments directory not found"
+    fi
+    
+    # Copy doc/tutorial-code directory
+    if [ -d ../../doc/tutorial-code ]; then
+      cp -r ../../doc/tutorial-code $out/share/doc/
+    else
+      echo "Warning: ../../doc/tutorial-code directory not found"
+    fi
+  '';
+}))
