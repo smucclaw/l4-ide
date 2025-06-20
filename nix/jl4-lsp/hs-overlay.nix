7e2abbb44f2b98b3766699ef21d78fe2229835e5
mangoiv <contact@mangoiv.com>
2025-06-20 19:11:34 +0200

    [chore] add websession to overlay

--- nix/jl4-lsp/hs-overlay.nix
+++ nix/jl4-lsp/hs-overlay.nix
@@ -1,6 +1,7 @@
 hlib: hself: hsuper: {
   megaparsec = hsuper.callHackage "megaparsec" "9.7.0" { };
   text-rope = hsuper.callHackage "text-rope" "0.3" { };
+  jl4-websessions = hlib.doJailbreak (hself.callCabal2nix "jl4-websessions" ../../jl4-websessions { });
   jl4-core = hlib.doJailbreak (hself.callCabal2nix "jl4-core" ../../jl4-core { });
   jl4-lsp = hlib.doJailbreak (hself.callCabal2nix "jl4-lsp" ../../jl4-lsp { });
 }
