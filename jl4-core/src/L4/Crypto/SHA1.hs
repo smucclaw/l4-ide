71e0247db091ff44ffcdf203a35f259786f82953
Thomas Gorissen <thomas.gorissen@me.com>
2026-01-21 18:06:42 +0800

    fix: Remove redundant foldl' imports for GHC 9.10 compatibility
    GHC 9.10+ exports foldl' from Prelude, making explicit imports from
    Data.List and Data.Foldable redundant. This fixes -Werror build failures.
    
    Files fixed:
    - jl4-core/src/L4/Crypto/SHA1.hs
    - jl4-lsp/src/LSP/Core/PositionMapping.hs
    - jl4-lsp/src/LSP/Core/Shake.hs
    
    Also includes regenerated WASM artifacts from merge.

--- jl4-core/src/L4/Crypto/SHA1.hs
+++ jl4-core/src/L4/Crypto/SHA1.hs
@@ -16,7 +16,6 @@ module L4.Crypto.SHA1
   ) where
 
 import Data.Bits
-import Data.List (foldl')
 import Data.Word
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
